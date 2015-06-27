7e0a802e24db53e2c91e276235f22396add47932
istobarton <istobarton@gmail.com>
2015-06-27 17:31:32 -0500

    Implemented like and fun UI

--- client/components/PetShopWindow.js
+++ client/components/PetShopWindow.js
@@ -42,13 +42,11 @@ PetShopWindow.like = function(userInfo, petID, shopID){
   var xhrConfig = function(xhr) {
     xhr.setRequestHeader("Content-Type", "application/json");
   }
-    return m.request({
+  return m.request({
     method: 'POST',
-    url: 'http://pet-shop.api.mks.io/shops/' + shopID + '/pets/' + petID +'/like',
+    url: 'http://pet-shop.api.mks.io/shops/1/pets/' + petID +'/like',
     config: xhrConfig,
-    apiToken : userInfo.apiToken
-
-    // { "username": username, "password": password},
+    data: { apiToken : userInfo.apiToken },
   })
 }
 
@@ -81,10 +79,10 @@ PetShopWindow.controller = function () {
   }
 
   ctrl.like = function(petID){
-    // debugger;
     PetShopWindow.like(ctrl.userInfo(), petID, ctrl.shop().id)
   }
 
+  setInterval(updatePets.bind(null, ctrl), 5000);
 }
 
 
@@ -99,8 +97,8 @@ PetShopWindow.view = function (ctrl) {
         m('br'),
         m('label', "Password:"),
         m('input[type=text]', {oninput: m.withAttr("value", ctrl.password), value: ctrl.password()}),
-        m('button', { onclick: ctrl.signUp }, 'Sign Up'),
-        m('button', { onclick: ctrl.signIn }, 'Sign In')
+        m('button', { class:'sign-up', onclick: ctrl.signUp }, 'Sign Up'),
+        m('button', { class:'sign-in', onclick: ctrl.signIn }, 'Sign In')
       ]),
     ]),
 
@@ -109,7 +107,7 @@ PetShopWindow.view = function (ctrl) {
         m('p', "Pet Name: " + pet.name),
         m('p', "species: " + pet.species),
         m('img', {src: pet.imageUrl, width: '75%', height: '55%'}),
-        m('p', "Likes: " + pet.likes),
+        m('p', "Likes: " + pet.likes.length),
         addLikeButton(ctrl, pet.id)
       ])
     })
@@ -120,78 +118,11 @@ PetShopWindow.view = function (ctrl) {
 function addLikeButton(ctrl, petID){
   if(ctrl.userInfo()){
     // debugger;
-    return m('button', { onclick: ctrl.like.papp(petID) }, "LIKE")
+    return m('button', { class: 'like', onclick: ctrl.like.papp(petID) }, "LIKE")
   }
 }
 
-// m('a', { onclick: ctrl.remove.papp(idx), href:'#' }, 'remove')
-
-// Contacts.view = function (ctrl) {
-
-//   return m('.contacts', [
-//     ctrl.contacts().map(function (contact, idx) {
-//       return m('fieldset', [
-//         m('legend', "Attendee #" + (idx+1)),
-//         m('label', "Name:"),
-//         m('input[type=text]', { value: contact.name(), onchange: m.withAttr('value', contact.name) }),
-//         m('br'),
-//         m('label', "Email:"),
-//         m('input[type=text]', { value: contact.email(), onchange: m.withAttr('value', contact.email) }),
-//         removeAnchor(ctrl, idx)
-//       ])
-//     }),
-//     m('a', { onclick: ctrl.add, href:'#' }, 'Add another attendee')
-//   ])
-// }
-
-// function removeAnchor (ctrl, idx) {
-//   if (ctrl.contacts().length >= 2) {
-//     return m('a', { onclick: ctrl.remove.papp(idx), href:'#' }, 'remove')
-//   }
-// }
-
-/*
-m("input", {onchange: m.withAttr("value", todo.vm.description), value: todo.vm.description()}),
-m("button", {onclick: todo.vm.add}, "Add"),*/
-
-/*
-ctrl.petShop().map(function (){
-  return
-})*/
-// + ctrl.petShop.name + ctrl.?. imageUrl + ctrl.petShop.likes + ctrl.petShop.id)
-// var m = require('mithril')
-// var Shop = require('../models/shop')
-
-
-// var PetShopWindow = module.exports = {}
-
-// PetShopWindow.controller = function () {
-//   var ctrl = this
-//   ctrl.shop = m.prop(null)
-//   Shop.fetch().then(ctrl.shop)
-// }
-
-// PetShopWindow.view = function (ctrl) {
-//   return m('.pet-shop', [
-//     m('h1', "Welcome to " + ctrl.shop().name)
-//   ])
-// }
-// Contacts.view = function (ctrl) {
-
-//   return m('.contacts', [
-//     m('h3', 'Please enter your contact information:'),
-//     ctrl.contacts().map(function (contact, idx) {
-//       return m('fieldset', [
-//         m('legend', "Attendee #" + (idx+1)),
-//         m('label', "Name:"),
-//         m('input[type=text]', { value: contact.name(), onchange: m.withAttr('value', contact.name) }),
-//         m('br'),
-//         m('label', "Email:"),
-//         m('input[type=text]', { value: contact.email(), onchange: m.withAttr('value', contact.email) }),
-//         removeAnchor(ctrl, idx)
-//       ])
-//     }),
-//     m('a', { onclick: ctrl.add, href:'#' }, 'Add another attendee')
-//   ])
-// }
+function updatePets(ctrl){
+  PetShopWindow.fetch().then(ctrl.petShop)
+}
 
